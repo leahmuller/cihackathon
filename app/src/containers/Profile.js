a859004412404c8abafb14d1bb15dddf5509e87b
Kris Merrill <kmerrill@gmail.com>
2020-07-15 18:02:31 -0700

    Reload NavBar after login

--- app/src/containers/Profile.js
+++ app/src/containers/Profile.js
@@ -1,44 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { Auth, Hub, Logger } from "aws-amplify";
+import { Auth, Logger } from "aws-amplify";
 import { withAuthenticator, SignOut } from "aws-amplify-react";
+import { useAppContext } from "../libs/contextLib";
 import Container from "../components/Container";
 import AmplifyTheme from "../AmplifyTheme";
 
 const logger = new Logger("Profile", "DEBUG");
 
-function Profile() {
+function Profile(props) {
+  const { setIsAuthenticating, userHasAuthenticated } = useAppContext();
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    Hub.listen("auth", (data) => {
-      logger.debug(
-        "A new auth event has happened: ",
-        data.payload.data.username + " has " + data.payload.event
-      );
-    });
-
     checkUser();
   }, []);
 
   async function checkUser() {
     try {
+      setIsAuthenticating(true);
       const data = await Auth.currentUserPoolUser();
       const userInfo = { username: data.username, ...data.attributes };
       setUser(userInfo);
+      userHasAuthenticated(true);
+      setIsAuthenticating(false);
+
+      // Testing 
+      logger.debug("User logged in and redirected");
       logger.debug(userInfo);
+      props.history.push("/home");
     } catch (err) {
       console.log("error: ", err);
     }
   }
 
   return (
-    <Container>
-      <h1>Profile</h1>
-      <h2>Username: {user.username}</h2>
-      <h3>Name: {user.name}</h3>
-      <h3>Email: {user.email}</h3>
-      <SignOut />
-    </Container>
+    <>
+      <Container>
+        <h1>Profile</h1>
+        <h2>Username: {user.username}</h2>
+        <h3>Name: {user.name}</h3>
+        <h3>Email: {user.email}</h3>
+        <SignOut />
+      </Container>
+    </>
   );
 }
 
